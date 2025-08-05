1ee44c474e31232cf35dae40df1432978600a0af
Shefaul Islam Shefa <sishefa19@gmail.com>
2025-08-05 12:24:47 +0600

    add list solution

--- problems/2025-08-03/List.py
+++ problems/2025-08-03/List.py
@@ -1,40 +1,40 @@
-# list = [2,3,4,5,8]
+list = [2,3,4,5,8]
 
-# list.append(7)
-# print(list)
+list.append(7)
+print(list)
 
-# list.insert(0,5)
-# print(list)
+list.insert(0,5)
+print(list)
 
-# list.remove(2)
-# print(list)
+list.remove(2)
+print(list)
 
-# list.pop()
-# print(list)
+list.pop()
+print(list)
 
-# del list[0]
-# print(list)
+del list[0]
+print(list)
 
-# list.clear()
-# print(list)
+list.clear()
+print(list)
 
-# newList = ["Shefa","Rakib","Shakib","Sajol"]
+newList = ["Shefa","Rakib","Shakib","Sajol"]
 
-# for i in range(len(newList)):
-#     print(newList[i],end =" ")
+for i in range(len(newList)):
+    print(newList[i],end =" ")
 
 
-# i = 0
-# while i < len(newList):
-#     print(newList[i], end = " ")
+i = 0
+while i < len(newList):
+    print(newList[i], end = " ")
     
-#     i += 1
+    i += 1
     
-# List Comprehension   
-# list = [1,2,3,4,5,6]
+List Comprehension   
+list = [1,2,3,4,5,6]
 
-# newList = [i*2 for i in list]
+newList = [i*2 for i in list]
 
-# print(newList)
+print(newList)
 
 
