f0140d2418772e6867b21586ffbd4989bc299738
Shefaul Islam Shefa <sishefa19@gmail.com>
2025-08-18 13:19:08 +0600

    add multilevel inheritance solutions

--- problems/2025-08-16/library_members_borrow_check.py
+++ problems/2025-08-16/library_members_borrow_check.py
@@ -18,3 +18,40 @@
 #
 # Example Output:
 # Member M301 (Arif) can borrow 2: True
+class Person:
+    pass
+
+class Member(Person):
+    def __init__(self, member_id, join_year):
+        super().__init__()
+        self.member_id = member_id
+        self.join_year = join_year
+        
+class StudentMember(Member):
+    def __init__(self,name, member_id, join_year, course_name, borrow_limit, requested_books):
+        super().__init__(member_id, join_year)
+        self.name = name
+        self.course_name = course_name
+        self.borrow_limit = borrow_limit
+        self.requested_books = requested_books
+        
+    def can_borrow(self):
+        return self.requested_books <= self.borrow_limit
+    
+    def __str__(self):
+        return f"Member {self.member_id} ({self.name}) can borrow {self.requested_books}: {self.can_borrow()}"
+    
+
+try:
+    name, member_id, join_year, course_name, borrow_limit, requested_books = input().split()
+    borrow_limit = int(borrow_limit)
+    requested_books = int(requested_books)
+    
+    if not name.isalpha() or not course_name.isalpha():
+        print("Please enter string")
+    else:
+        obj = StudentMember(name, member_id, join_year, course_name, borrow_limit, requested_books)
+        print(obj)
+    
+except ValueError:
+    print("Please insert valid data.")
