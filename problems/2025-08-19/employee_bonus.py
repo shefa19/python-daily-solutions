4f03fdba46b2f5c42289ad33338f2ecec65b7e32
Shefaul Islam Shefa <sishefa19@gmail.com>
2025-08-19 23:48:08 +0600

    add polymorphism solutions

--- problems/2025-08-19/employee_bonus.py
+++ problems/2025-08-19/employee_bonus.py
@@ -18,3 +18,58 @@
 #
 # Example Output:
 # Bonus: 8000
+
+class Employee:
+    def __init__(self, salary):
+        self.salary = salary
+        
+    def bonus(self):
+        pass
+
+class Manager(Employee):
+    def __init__(self, salary):
+        super().__init__(salary)
+        
+    def bonus(self):
+        return self.salary * 25 // 100
+        
+
+class Developer(Employee):
+    def __init__(self, salary):
+        super().__init__(salary)
+        
+    def bonus(self):
+        return self.salary * 20 // 100
+        
+
+class Intern(Employee):
+    def __init__(self, salary):
+        super().__init__(salary)
+        
+    def bonus(self):
+        return self.salary * 10 // 100
+    
+
+try:
+    role, salary = input("Enter role and salary: ").split()
+    salary = int(salary)
+
+    if not role.isalpha():
+        print("Please enter valid role.")
+    else:
+        obj = None
+        match role.lower():
+            case "manager":
+                obj = Manager(salary)
+            case "developer":
+                obj = Developer(salary)
+            case "intern":
+                obj = Intern(salary)
+            case _:
+                print("Unknown role")
+
+except ValueError:
+    print("Please enter valid value.")
+
+if obj:
+    print(f"Bonus: {obj.bonus()}")
